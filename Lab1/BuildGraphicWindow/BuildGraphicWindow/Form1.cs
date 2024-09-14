fb47454b19941694b130b574deaf377fc212e9ad
Егор Деркунский <114829512+Palom-Porom@users.noreply.github.com>
2024-09-14 14:28:54 +0300

    Added file for Parsing Expressions

--- Lab1/BuildGraphicWindow/BuildGraphicWindow/Form1.cs
+++ Lab1/BuildGraphicWindow/BuildGraphicWindow/Form1.cs
@@ -7,14 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using CodingSeb.ExpressionEvaluator;
 
 namespace BuildGraphicWindow
 {
     public partial class Form1 : Form
     {
+        //public Form1()
+        //{
+        //    InitializeComponent();
+        //}
+
+
+        //===========================================
+        //===========================================
+
+        private Graphics g;
+
         public Form1()
         {
             InitializeComponent();
+            g = this.CreateGraphics();
+            //this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
         }
+         
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Pen pen = new Pen(Color.SlateBlue);
+            SolidBrush solid = new SolidBrush(Color.Red);
+            g.FillEllipse(solid, e.X, e.Y, 5, 5);
+            g.DrawEllipse(pen, e.X, e.Y, 5, 5);
+
+            solid.Dispose();
+            pen.Dispose();
+        }
+
+        //===========================================
+        //===========================================
+
+        //private Graphics g;
+        //private Image im;
+        //private PictureBox pictureBox1;
+
+        //public Form1()
+        //{
+        //    InitializeComponent();
+        //    this.pictureBox1 = new PictureBox();
+        //    pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        //    g = Graphics.FromImage(pictureBox1.Image);
+        //    g.Clear(Color.White);
+
+        //    //this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+        //    //this.MouseDoubleClick += button1_Click;
+        //}
+
+        //private void Form1_MouseClick(object sender, MouseEventArgs e)
+        //{
+        //    Pen pen = new Pen(Color.SlateBlue);
+        //    SolidBrush solid = new SolidBrush(Color.Red);
+        //    g.FillEllipse(solid, e.X, e.Y, 5, 5);
+        //    g.DrawEllipse(pen, e.X, e.Y, 5, 5);
+
+        //    solid.Dispose();
+        //    pen.Dispose();
+        //    pictureBox1.Invalidate();
+
+        //}
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    pictureBox1.Image.Save("image1.png");
+        //}
     }
 }
